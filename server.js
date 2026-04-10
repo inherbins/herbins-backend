f4d413d270fdc8071fee3d8c813bca6a8fc0996b
inherbins <inherbins@gmail.com>
2026-04-10 16:28:44 +0530

    fixed PORT syntax error

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post("/save-order", (req, res) => {
 });
 
 // ✅ Start server
-aconst PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
