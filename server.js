866dc6a4fa7b72b17cc8f1ad3282da5bd64fec85
inherbins <inherbins@gmail.com>
2026-04-10 18:19:44 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const mysql = require("mysql2");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(bodyParser.json());
 
 
